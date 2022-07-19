600971f3320a7d8f9e04064911c0fc8eeb0f0008
sbchild <a15315862177@163.com>
2022-07-19 18:48:14 +0800

    commit

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ def slash(message: str, my_name: str, other_name: str, other_sent: bool):
         message_list[0] += "了"
     if len(message_list) == 2:
         if message_list[0].startswith("被"):
-            message_list[1] += "了"
+            if not message_list[0].endswith("了"):
+                message_list[1] += "了"
         elif not message_list[1].startswith("的"):
             message_list[1] = "的" + message_list[1]
         message_list[1] = " " + message_list[1]
